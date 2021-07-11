a36c70de13c7ea145bd039c31be6b6b802c10847
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-07-11 17:10:44 -0600

    added default values

--- harrysbook/packages/local-api/src/routes/cells.ts
+++ harrysbook/packages/local-api/src/routes/cells.ts
@@ -14,12 +14,20 @@ export const createCellsRouter = (filename: string, dir: string) => {
 	const fullPath = path.join(dir, filename);
 	
 	router.get("/cells", async (req, res) => {
-		// Make sure the cell storage exists
-		// If it does not exist, add in a default list of cells.
-		
-		// Read the file
-		// Parse a list of cells out of it
-		// Send list of	cells back to browser
+		try {
+			const result = await fs.readFile(fullPath, { encoding: "utf-8" });
+
+			res.send(JSON.parse(result));
+		} catch (err) {
+			// If read throws an error, inspect the error, check if it says the file does not exist
+
+			if(err.code === "ENOENT") {
+				await fs.writeFile(fullPath, "[]", "utf-8");
+				res.send([]);
+			} else {
+				throw err;
+			}
+		}
 	});
 	
 	router.post("/cells", async (req, res) => {
