9c6b7646f92f8d97dbab3ecfd6c041e061674b2c
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-07-11 20:10:25 -0600

    updated

--- tiny-npm-deploy/src/index.ts
+++ tiny-npm-deploy/src/index.ts
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("hi, there!!!!!!!!!!!!!");
+app.get('/', (req, res) => {
+  res.send('hi there!');
 });
 
 app.listen(3005, () => {
-	console.log("Listening on 3005");
+  console.log('Listening on 3005');
 });
+
